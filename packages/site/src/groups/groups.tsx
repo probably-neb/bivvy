3a8b30ebd0d94cdaf20cb7d3cbd091f913f8e9b0
Ben Kunkle <ben.kunkle@gmail.com>
2024-01-06 12:55:08 -0800

    group card

--- packages/site/src/groups/groups.tsx
+++ packages/site/src/groups/groups.tsx
@@ -1,12 +1,31 @@
-import { useGroups } from "@/lib/rep"
-import { For } from "solid-js"
+import { UserRenderer } from "@/components/renderers"
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card"
+import { Group, useGroups, useOtherUsers, useUsers } from "@/lib/rep"
+import { routes } from "@/routes"
+import { A } from "@solidjs/router"
+import { For, createEffect, createMemo } from "solid-js"
 
 export default function Groups() {
     const groups = useGroups()
     return <div>
         Groups
         <For each={groups()}>
-            {group => <div>{group.name}</div>}
+            {group => <GroupCard group={group} />}
         </For>
     </div>
+}
+
+function GroupCard(props: {group: Group}) {
+    const groupId = createMemo(() => props.group.id)
+    // TODO: useOwed instead and show owed (mimick overview card in groups page)
+    const otherUsers = useOtherUsers(groupId)
+    createEffect(() => console.log("other users", otherUsers()?.map(u => u.name)))
+    return <Card class="max-w-sm">
+        <CardContent class="pt-2">
+            <CardTitle class="text-xl"><A href={routes.group(props.group.id)}>{props.group.name}</A></CardTitle>
+            <For each={otherUsers()}>
+                {user => <UserRenderer userId={user.id} groupId={groupId} />}
+            </For>
+        </CardContent>
+    </Card>
 }
