86fb6a12a95620b2376b75d53e80523b986aca0f
Ben Kunkle <ben.kunkle@gmail.com>
2024-01-06 16:49:05 -0800

    drop data button

--- packages/site/src/lib/layout.tsx
+++ packages/site/src/lib/layout.tsx
@@ -3,6 +3,7 @@ import { fade } from "@/lib/fade";
 import { useRep } from "@/lib/rep";
 import { Show } from "solid-js";
 import { Button } from "@/components/ui/button";
+import {dropAllDatabases} from "replicache"
 
 export default function Layout({ children }: ParentProps) {
     // TODO: current user hook (and store current user at known key in rep)
@@ -17,7 +18,10 @@ export default function Layout({ children }: ParentProps) {
                         <h1 class="text-4xl font-bold">Paypals</h1>
                     </div>
                     <Show when={import.meta.env.VITE_IS_LOCAL}>
-                        <ForcePullButton />
+                        <div class="flex justify-evenly gap-2">
+                            <ForcePullButton />
+                            <DropDataButton />
+                        </div>
                     </Show>
                 </div>
                 {children}
@@ -37,4 +41,16 @@ function ForcePullButton() {
     return <Show when={rep()}>
         <Button variant="outline" onClick={onClick}>Pull</Button>
     </Show>
+}
+
+function DropDataButton() {
+    // TODO: reinit rep after drop
+    const onClick = async () => {
+        const dropped = await dropAllDatabases()
+        console.log("dropped", dropped.dropped)
+        if (dropped.errors.length > 0) {
+            console.error("errors", dropped.errors)
+        }
+    }
+    return <Button variant="outline" onClick={onClick}>Drop</Button>
 }
