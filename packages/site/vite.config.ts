768a198df89d07b1bb585338c162c0f629e9ad81
Ben Kunkle <ben.kunkle@gmail.com>
2024-01-21 23:32:47 -0800

    node resolve for rollup

--- packages/site/vite.config.ts
+++ packages/site/vite.config.ts
@@ -1,13 +1,19 @@
-import { defineConfig } from 'vite'
-import solid from 'vite-plugin-solid'
-import path from 'path'
+import { defineConfig } from "vite";
+import solid from "vite-plugin-solid";
+import path from "path";
+import { nodeResolve } from '@rollup/plugin-node-resolve';
 
 export default defineConfig({
-  plugins: [solid()],
+    plugins: [solid()],
     resolve: {
         alias: {
             "@": path.resolve(__dirname, "./src"),
-            "solid": "solid-js",
+            solid: "solid-js",
+        },
+    },
+    build: {
+        rollupOptions: {
+            plugins: [nodeResolve]
         }
     }
-})
+});
