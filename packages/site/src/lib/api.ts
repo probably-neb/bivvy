5a8fc743bbb66ea4ae447e2daa5039eed389e881
Ben Kunkle <ben.kunkle@gmail.com>
2024-01-23 15:27:18 -0800

    custom fetch in api

--- packages/site/src/lib/api.ts
+++ packages/site/src/lib/api.ts
@@ -1,45 +1,88 @@
 import { untrack } from "solid-js";
 import { Group } from "./rep";
 import { useToken } from "./session";
-import { intoMap } from "./utils";
-import { z } from "zod";
+import { ZodType, z } from "zod";
 
 export * as Api from "./api";
 
 const url = import.meta.env.VITE_API_URL;
 
+type QueryParams = ConstructorParameters<typeof URLSearchParams>[0];
+
+type FetchOpts<T> = RequestInit & {
+    params?: QueryParams;
+    validator?: ZodType<T>;
+};
+
+const raiseForStatus = (response: Response) => {
+    if (!response.ok) {
+        throw new Error(response.statusText);
+    }
+    return response;
+};
+async function fetch<T>(path: string, init?: FetchOpts<T>): Promise<T> {
+    if (!path.startsWith("/")) {
+        path = `/${path}`;
+    }
+    if (!path.startsWith(url)) {
+        path = `${url}${path}`;
+    }
+    if (init?.params) {
+        const params = new URLSearchParams(init.params);
+        path = `${path}?${params.toString()}`;
+    }
+    const token = untrack(() => useToken()());
+    if (token) {
+        if (!init) {
+            init = {};
+        }
+        init.headers = Object.assign(
+            {
+                authorization: `Bearer ${token}`,
+            },
+            init.headers,
+        );
+    }
+    const res = await window.fetch(path, init);
+    raiseForStatus(res);
+    const json = await res.json();
+    if (init?.validator) {
+        return init.validator.parse(json);
+    }
+    return json;
+}
+
 export async function getInviteKey(groupId: Group["id"]) {
     if (!groupId) {
         throw new Error("Missing groupId");
     }
-    const query = new URLSearchParams();
-    query.set("groupId", groupId);
-    const authToken = untrack(() => useToken()())
-    if (!authToken) {
-        throw new Error("Missing auth token");
-    }
-    // TODO: raise for status
-    const response = await fetch(`${url}/invite?groupId=${groupId}`, {
-        headers: {
-            authorization: `Bearer ${authToken}`,
-        }
-    }).then(r => r.json());
-    // TODO: validate
-    return response.token as string
+    const response = await fetch("/invite", {
+        validator: z.object({
+            token: z.string(),
+        }),
+        params: {
+            groupId,
+        },
+    });
+    return response.token;
 }
 
 export async function validateInviteKey(key: string) {
-    const response = await fetch(`${url}/invite/validate?token=${key}`).then(r => r.json());
+    const response = await fetch("/invite/validate", {
+        params: { token: key },
+        // FIXME: validate
+        validator: z.object({}),
+    });
     return response;
 }
 
-
-const zProviders = z.record(z.string().url())
+const zProviders = z.record(z.string().url());
 
 export async function getAuthProviders() {
-    const res = await fetch(`${url}/auth/`)
-    const providers = await res.json();
-    return zProviders.parse(providers);
+    const res = await fetch("/auth/", {
+        validator: zProviders,
+    });
+    return res;
 }
 
 export function authUrl(provider: string) {
