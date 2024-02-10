e0217fa391263663748dba7f02089633300b258b
Ben Kunkle <ben.kunkle@gmail.com>
2024-02-10 10:12:47 -0800

    clean up jwt parsing code

--- packages/functions/db/jwt.go
+++ packages/functions/db/jwt.go
@@ -34,41 +34,59 @@ func (s PublicSession) SessionType() SessionType {
 
 var signingMethod = jwt.SigningMethodRS512
 
+func getPublicKey(token *jwt.Token) (any, error) {
+    if _, ok := token.Method.(*jwt.SigningMethodRSA); !ok {
+        return nil, fmt.Errorf("unexpected signing method: %v", token.Header["alg"])
+    }
+    publicKeyPem, err := GetJWTPublicKey()
+    if (err != nil) {
+        log.Fatalf("Could not get jwt public key: %v", err)
+    }
+    publicKey, err := jwt.ParseRSAPublicKeyFromPEM([]byte(publicKeyPem))
+    return publicKey, err
+}
+
+func getSessionFromClaims(claims jwt.MapClaims) (Session, error) {
+    sessionType, ok := claims["type"].(string)
+    if !ok {
+        return nil, fmt.Errorf("jwt claims did not contain session type: %v", claims)
+    }
+
+    properties, ok := claims["properties"].(map[string]any)
+    if !ok {
+        return nil, fmt.Errorf("jwt claims did not contain properties: %v", claims)
+    }
+
+    var session Session = PublicSession{}
+
+    if sessionType == "user" {
+        userId, ok := properties["userId"].(string)
+        if !ok {
+            err := fmt.Errorf("jwt claims did not contain userId for user session: %v", claims)
+            return nil, err
+        }
+        session = UserSession{UserId: userId}
+    }
+
+    return session, nil
+}
+
 func GetSession(tokenString string) Session {
-    token, err := jwt.Parse(tokenString, func (token *jwt.Token) (any, error) {
-        if _, ok := token.Method.(*jwt.SigningMethodRSA); !ok {
-            return nil, fmt.Errorf("Unexpected signing method: %v", token.Header["alg"])
-        }
-        publicKeyPem, err := GetJWTPublicKey()
-        if (err != nil) {
-            log.Fatalf("Could not get jwt public key: %v", err)
-        }
-        publicKey, err := jwt.ParseRSAPublicKeyFromPEM([]byte(publicKeyPem))
-        return publicKey, nil
-    })
+    token, err := jwt.Parse(tokenString, getPublicKey)
     if err != nil {
         log.Fatalf("Could not parse jwt: %v", err)
     }
 
-    var session Session
     claims, ok := token.Claims.(jwt.MapClaims);
     if !ok || !token.Valid {
         log.Fatalf("Could not parse jwt claims: ok=%v, valid=%v",ok, token.Valid)
     }
-    properties, ok := claims["properties"].(map[string]interface{})
-    if !ok {
-        log.Fatalf("jwt claims did not contain properties: %v", claims)
+
+    session, err := getSessionFromClaims(claims)
+    if err != nil {
+        log.Fatalf("Could not get session from claims: %v", err)
     }
-    switch claims["type"] {
-    case "user":
-        userId, ok := properties["userId"].(string)
-        if !ok {
-            log.Fatalf("jwt claims did not contain expected session info: %v", claims)
-        }
-        session = UserSession{UserId: userId}
-    default:
-        session = PublicSession{}
-    }
+
     return session
 }
 
@@ -76,10 +94,10 @@ func GetSessionFromHeaders(headers map[string]string) (Session, error) {
     var session Session = PublicSession{}
     header, ok := headers["authorization"]
     if !ok {
-        return session, fmt.Errorf("Request did not contain Authorization header")
+        return session, fmt.Errorf("request did not contain Authorization header")
     }
     if len(header) < 7 {
-        return session, fmt.Errorf("Authorization header was too short")
+        return session, fmt.Errorf("authorization header was too short")
     }
     token := header[7:]
     // for some reason Replicache does not include the Bearer prefix
