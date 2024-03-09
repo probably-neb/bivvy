c4104be1c43c729e654cdab325b985ad2a13c576
Ben Kunkle <ben.kunkle@gmail.com>
2024-03-09 15:32:40 -0800

    try getting drizzle-kit studio to work (not working)

--- packages/db/drizzle.config.ts
+++ packages/db/drizzle.config.ts
@@ -1,12 +1,18 @@
-import {Config} from "drizzle-kit"
+import {Config as DrizzleConfig} from "drizzle-kit"
 
-const DB_URL = process.env.DB_URL!.replace("sslaccept=strict", 'ssl={"rejectUnauthorized":true}')
-
+console.log(process.env)
+const DB_TOKEN = process.env.DB_TOKEN!;
+let DB_URL = process.env.DB_URL!;
+if (DB_URL.startsWith("http")) {
+    DB_URL = DB_URL.replace("https", "libsql")
+}
+console.log({DB_URL, DB_TOKEN})
 export default {
     schema: "./src/schema.ts",
     out: "./migrations",
-    driver: "mysql2",
+    driver: "turso",
     dbCredentials: {
-        uri: DB_URL,
+        url: DB_URL!,
+        authToken: DB_TOKEN!
     }
-} satisfies Config;
+} satisfies DrizzleConfig;
