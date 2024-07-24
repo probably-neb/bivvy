cbf734aa2d60e8d27aa9645f1de9b15586b64d4d
Ben Kunkle <ben.kunkle@gmail.com>
2024-07-24 12:23:35 -0500

    fix keep alive libsql deps

--- stacks/DB.ts
+++ stacks/DB.ts
@@ -5,9 +5,15 @@ export function DB({stack}: StackContext) {
     const DB_TOKEN = new Config.Secret(stack, "DB_TOKEN");
 
     const keepAlive = new Cron(stack, "KeepAlive", {
-        schedule: "rate(5 days)",
+        schedule: "rate(1 minute)",
         job: new Function(stack, "KeepAliveLambda", {
             handler: "packages/functions/lambdas/db/keep-alive.handler",
+            runtime: "nodejs20.x",
+            architecture: "x86_64",
+            nodejs: {
+                install: ["@libsql/client", "@libsql/linux-x64-gnu"],
+            },
+            enableLiveDev: false,
             bind: [DB_URL, DB_TOKEN],
         }),
     })
