99f657c454ad695611844bc514d5c367fbd26378
Ben Kunkle <ben.kunkle@gmail.com>
2024-09-16 11:42:39 -0500

    first draft of receipt upload ui

--- packages/site/src/scan/receipt.tsx
+++ packages/site/src/scan/receipt.tsx
@@ -3,25 +3,42 @@ import {
     createResource,
     Show,
     Index,
+    JSX,
+    createEffect,
+    splitProps,
+    For,
+    batch,
+    ParentProps,
 } from "solid-js";
 import { Api } from "@/lib/api";
 import { Card, CardContent, CardHeader } from "@/components/ui/card";
-import { Input } from "@/components/ui/input";
+import LoadingSpinner from "@/components/loading-spinner";
+import { TiUpload } from "solid-icons/ti";
+import {
+    Dialog,
+    DialogContent,
+    DialogTitle,
+    DialogTrigger,
+} from "@/components/ui/dialog";
+import { Button } from "@/components/ui/button";
+import { cn } from "@/lib/utils";
+import LabeledInput from "@/components/ui/labeled-input";
+import { ToggleButton } from "@/components/ui/toggle";
+import { createStore } from "solid-js/store";
 
 // TODO: support document extract reciept from image
 // https://github.com/ColonelParrot/jscanify
 
-type ReceiptInfo = Awaited<ReturnType<typeof Api.scanReceipt>>
+type ReceiptInfo = Awaited<ReturnType<typeof Api.scanReceipt>>;
 
-type ReceiptInfoItem = ReceiptInfo['items'][number]
+type ReceiptInfoItem = ReceiptInfo["items"][number];
 
 export default function Scan() {
     const [file, setFile] = createSignal<File | null>(null);
-    const [showModal, setShowModal] = createSignal(false);
-    const [res, { refetch }] = createResource(file, async (file) => {
-        const res =  await Api.scanReceipt(file)
-        console.log(res);
-        return res;
+    const [res, { refetch }] = createResource(file, Api.scanReceipt);
+
+    createEffect(() => {
+        console.log("info:", res());
     });
 
     function onUpload(files: FileList | null) {
@@ -35,67 +52,125 @@ export default function Scan() {
             );
         }
         setFile(files[0]);
-        setShowModal(false);
     }
 
     return (
-        <div class="container mx-auto p-4">
-            <button
-                class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
-                onClick={[setShowModal, true]}
-            >
-                Upload Receipt
-            </button>
-
-            <Show when={showModal()}>
-                (
-                <div class="fixed inset-0 bg-gray-600 bg-opacity-50 overflow-y-auto h-full w-full flex items-center justify-center">
-                    <div class="bg-white p-5 rounded-lg shadow-xl">
-                        <h2 class="text-xl mb-4">Upload Receipt</h2>
-                        <input
-                            type="file"
-                            accept="image/*"
-                            capture="environment"
-                            onChange={(e) => onUpload(e.target.files)}
-                            class="mb-4"
-                        />
-                        <button
-                            class="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded"
-                            onClick={[setShowModal, false]}
+        <div class="container mx-auto p-4 bg-background w-full h-full ring-2 ring-foreground">
+            <Show when={file() == null}>
+                <FileUpload onFileUpload={onUpload} />
+            </Show>
+            <Show when={file()}>
+                {(file) => (
+                    <div class="h-full w-full">
+                        <Show
+                            when={res()}
+                            fallback={
+                                <>
+                                    <div class="h-[10%]">
+                                        <RecieptPreview reciept={file()} />
+                                    </div>
+                                    <div class="h-[90%]">
+                                        <Show when={res.error}>
+                                            <div class="text-center my-4 text-red-500">
+                                                Error: {res.error.message}
+                                            </div>
+                                            <button onClick={[refetch, file()]}>
+                                                Refetch
+                                            </button>
+                                        </Show>
+                                        <Show when={res.loading}>
+                                            <LoadingSpinner />
+                                        </Show>
+                                    </div>
+                                </>
+                            }
                         >
-                            Cancel
-                        </button>
+                            {(res) => (
+                                <ReceiptInfoEdit info={res()}>
+                                    <RecieptPreview reciept={file()} />
+                                    <JsonPreview json={res()} />
+                                </ReceiptInfoEdit>
+                            )}
+                        </Show>
+                        <div class="h-[10%]"></div>
+                        <div class="h-[90%]"></div>
                     </div>
-                </div>
-                )
+                )}
             </Show>
-
-            <Show when={res.loading}>
-                <div class="text-center my-4">Loading...</div>
-            </Show>
-
-            <Show when={res.error}>
-                <div class="text-center my-4 text-red-500">
-                    Error: {res.error.message}
-                </div>
-            </Show>
-            {file() && res() && (
-                <div class="flex flex-col md:flex-row gap-4 mt-4">
-                    <section class="self-last md:self-first w-full md:w-1/2">
-                        <h2 class="text-xl mb-2">Receipt Preview</h2>
-                        <button onClick={() => refetch()}>Refetch</button>
-                        <RecieptPreview reciept={file()} />
-                        <pre class="bg-gray-100 p-4 rounded overflow-x-auto">
-                            {JSON.stringify(res(), null, 2)}
-                        </pre>
-                    </section>
-                    <section class="w-full md:w-1/2">
-                        <h2 class="text-xl mb-2">Scanned Data</h2>
-                            <ReceiptItems items={res()!.items} />
-                    </section>
-                </div>
-            )}
         </div>
+    );
+}
+
+function FileUpload({
+    onFileUpload,
+}: {
+    onFileUpload: (files: FileList | null) => void;
+}) {
+    const onDragOver: JSX.EventHandlerUnion<HTMLLabelElement, DragEvent> = (
+        event
+    ) => {
+        event.preventDefault();
+        // event.persist();
+        event.stopPropagation();
+
+        const hasFiles = isEventWithFiles(event);
+        if (hasFiles && event.dataTransfer) {
+            try {
+                event.dataTransfer.dropEffect = "copy";
+            } catch {} /* eslint-disable-line no-empty */
+        }
+
+        return false;
+    };
+
+    const onDrop: JSX.EventHandlerUnion<HTMLLabelElement, DragEvent> = (e) => {
+        e.preventDefault();
+        e.stopPropagation();
+        if (e.dataTransfer) {
+            onFileUpload(e.dataTransfer.files);
+        }
+    };
+    const onChange: JSX.ChangeEventHandlerUnion<HTMLInputElement, Event> = (
+        e
+    ) => onFileUpload(e.target.files);
+
+    return (
+        <div class="grid gap-4">
+            <label
+                for="file"
+                class="flex h-40 w-full items-center justify-center border-2 border-dashed border-foreground px-6 py-10 transition-colors hover:border-primary relative"
+                ondragover={onDragOver}
+                ondrop={onDrop}
+            >
+                <div class="absolute select-none font-medium text-foreground top-0 left-4 ring-2 ring-foreground -translate-y-1/2 bg-background px-2">
+                    UPLOAD RECEIPT
+                </div>
+                <div class="absolute flex flex-col items-center space-y-2">
+                    <TiUpload class="h-12 w-12 text-foreground" />
+                </div>
+            </label>
+            <input
+                type="file"
+                id="file"
+                name="file"
+                class="sr-only"
+                onChange={onChange}
+                accept="image/*"
+            />
+        </div>
+    );
+}
+
+function isEventWithFiles(event: any) {
+    if (event == null) return false;
+    if (!event.dataTransfer) {
+        return !!event.target && !!event.target.files;
+    }
+    // https://developer.mozilla.org/en-US/docs/Web/API/DataTransfer/types
+    // https://developer.mozilla.org/en-US/docs/Web/API/HTML_Drag_and_Drop_API/Recommended_drag_types#file
+    return Array.prototype.some.call(
+        event.dataTransfer.types,
+        (type) => type === "Files" || type === "application/x-moz-file"
     );
 }
 
@@ -105,28 +180,191 @@ function RecieptPreview(props: { reciept: File | null }) {
     }
     const blob = URL.createObjectURL(props.reciept);
 
+    const img = (
+        <img class="w-full object-contain" src={blob} alt="Receipt preview" />
+    );
+    const [open, setOpen] = createSignal(false);
+
     return (
-        <div class="max-h-[500px] overflow-y-auto">
-            <img
-                class="w-full object-contain"
-                src={blob}
-                alt="Receipt preview"
-            />
-        </div>
+        <Dialog open={open()} onOpenChange={setOpen}>
+            <DialogTrigger>
+                <Button>RECEIPT</Button>
+            </DialogTrigger>
+            <DialogContent class="max-w-[80%] max-h-[80%] ring-2 ring-foreground">
+                <DialogTitle variant="label">RECEIPT</DialogTitle>
+                <div class="h-full w-full overflow-y-auto overflow-x-auto">
+                    {img}
+                </div>
+            </DialogContent>
+        </Dialog>
     );
 }
 
-function ReceiptItems(props: {items: Array<ReceiptInfoItem>}) {
-    return <Index each={props.items}>
-        {item => (
-            <Card>
-                <CardHeader>
-                    <Input value={item().name.value} />
-                </CardHeader>
-                <CardContent>
-                    <Input value={item().price.value} />
-                </CardContent>
-            </Card>
-        )}
-    </Index>
+function JsonPreview(props: { json: any | null }) {
+    if (props.json == null) {
+        return null;
+    }
+
+    const [open, setOpen] = createSignal(false);
+
+    return (
+        <Dialog open={open()} onOpenChange={setOpen}>
+            <DialogTrigger>
+                <Button>{"{} "}JSON</Button>
+            </DialogTrigger>
+            <DialogContent class="max-w-[80%] max-h-[80%] ring-2 ring-foreground">
+                <DialogTitle variant="label">JSON</DialogTitle>
+                <pre class="h-full max-h-full w-full bg-gray-100 p-4 rounded overflow-x-auto overflow-y-auto">
+                    {JSON.stringify(props.json, null, 2)}
+                </pre>
+            </DialogContent>
+        </Dialog>
+    );
+}
+
+function ReceiptInfoEdit(props: ParentProps<{ info: ReceiptInfo }>) {
+    const [items, setItems] = createStore<ReceiptInfoItem[]>(props.info.items);
+    type ReceiptInfoGroup = {
+        splitID: string | null;
+        items: Array<ReceiptInfoItem>;
+    };
+    const [groups, setGroups] = createStore<ReceiptInfoGroup[]>([]);
+    const [selected, setSelected] = createSignal<Array<number>>([]);
+
+    function createGroupFromSelected() {
+        const selected_ = selected();
+        if (selected_.length == 0) {
+            return;
+        }
+        if (items.length < selected_.length) {
+            console.error("Not enough items");
+        }
+
+        const selectedItems = items.filter((_, i) => selected_.includes(i));
+        const unselectedItems = items.filter((_, i) => !selected_.includes(i));
+        const group = {
+            splitID: null,
+            items: selectedItems,
+        };
+
+        batch(() => {
+            setGroups((groups) => [...groups, group]);
+            setItems(unselectedItems);
+            setSelected([]);
+        });
+    }
+
+    function confidenceIndicatorClass(
+        confidence: number,
+        touched: boolean = false
+    ) {
+        let color = "bg-green-500";
+        if (!touched) {
+            if (confidence < 1 && confidence > 0) {
+                confidence = confidence * 100;
+            }
+
+            if (confidence < 25) {
+                color = "bg-red-500";
+            } else if (confidence < 50) {
+                color = "bg-orange-500";
+            } else if (confidence < 75) {
+                color = "bg-yellow-500";
+            }
+        }
+
+        return "w-2 h-2 p-0 rounded-full " + color;
+    }
+
+    return (
+        <>
+            <div class="h-[10%]">
+                {props.children}
+
+                <Button
+                    disabled={selected().length == 0}
+                    onClick={createGroupFromSelected}
+                >
+                    GROUP SELECTED
+                </Button>
+            </div>
+            <section class="h-[90%] flex flex-col md:flex-row gap-4 p-2 overflow-y-auto w-full md:w-1/2">
+                <Index each={groups}>
+                    {(group) => (
+                        <Card>
+                            <CardHeader>
+                                <LabeledInput label="DESCRIPTION" />
+                            </CardHeader>
+                            <CardContent class="space-y-4">
+                                <Index each={group().items}>
+                                    {(item) => (
+                                        <>
+                                            <LabeledInput
+                                                value={item().name.value}
+                                                label="ITEM"
+                                                rightLabel={null}
+                                                rightLabelClass={confidenceIndicatorClass(
+                                                    item().name.confidence
+                                                )}
+                                            />
+                                            <LabeledInput
+                                                value={item().price.value}
+                                                label="PRICE"
+                                                rightLabel={null}
+                                                rightLabelClass={confidenceIndicatorClass(
+                                                    item().price.confidence
+                                                )}
+                                            />
+                                        </>
+                                    )}
+                                </Index>
+                            </CardContent>
+                        </Card>
+                    )}
+                </Index>
+                <Index each={items}>
+                    {(item, index) => (
+                        <Card>
+                            <CardHeader>
+                                <ToggleButton
+                                    pressed={selected().includes(index)}
+                                    onChange={(pressed) => {
+                                        if (!pressed) {
+                                            setSelected(
+                                                selected().filter(
+                                                    (i) => i != index
+                                                )
+                                            );
+                                        } else {
+                                            setSelected([...selected(), index]);
+                                        }
+                                    }}
+                                >
+                                    SELECT
+                                </ToggleButton>
+                            </CardHeader>
+                            <CardContent class="space-y-8">
+                                <LabeledInput
+                                    value={item().name.value}
+                                    label="ITEM"
+                                    rightLabel={null}
+                                    rightLabelClass={confidenceIndicatorClass(
+                                        item().name.confidence
+                                    )}
+                                />
+                                <LabeledInput
+                                    value={item().price.value}
+                                    label="PRICE"
+                                    rightLabel={null}
+                                    rightLabelClass={confidenceIndicatorClass(
+                                        item().price.confidence
+                                    )}
+                                />
+                            </CardContent>
+                        </Card>
+                    )}
+                </Index>
+            </section>
+        </>
+    );
 }
